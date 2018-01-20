02e3031b49b893625ba9f5139f71b8a5801549f4
heathercoraje <yhk7830@hotmail.com>
2018-01-20 14:12:01 +0900

    test-promise

--- public/request.js
+++ public/request.js
@@ -1,13 +1,22 @@
-fetch(url, {
-  method: 'GET'
-}).then(function(response) {
-  return response.json();
-}).then(function(result){
-    console.log(result);
-  }).catch(function(err){
-  console.log('there is error: ', error);
-});
+// fetch(url, {
+//   method: 'GET'
+// }).then(function(response) {
+//   return response.json();
+// }).then(function(result){
+//     console.log(result);
+//   }).catch(function(err){
+//   console.log('there is error: ', error);
+// });
 
 
+var testUrl = 'https://api.github.com/users/heathercoraje/repos';
 
 // https://www.sitepoint.com/html5-geolocation/
+fetch(testUrl, {
+  method: 'GET'
+}).then(function(response) {
+  var data = response.json();
+  console.log(data)
+}).catch(function(error){
+  console.log('there is error: ', error);
+});
