3bacffb10fd224d7ed991bb76da0c2700fdf356a
heathercoraje <yhk7830@hotmail.com>
2018-01-20 22:28:34 +0900

    add working code

--- public/index.js
+++ public/index.js
@@ -1,16 +1,54 @@
-// get current location
 
-(function getLocation () {
-  if (navigator.geolocation){
-    console.log('yes your browswer supports this function');
-    navigator.geolocation.getCurrentPosition(showPosition);
+(function() {
+  function getHour() {
+    var time = new Date().toLocaleTimeString();
+    document.getElementById('time').innerHTML = time;
+  }
+  getHour();
+  var myTimer = setInterval(getHour, 1000);
 
+  var endpoint = 'https://fcc-weather-api.glitch.me/api/current?';
+  (function getLocation() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log('Your browser does not support getLocation function');
+    }
+  })();
+
+  function showPosition (position) {
+    var lat = 'lat=' + position.coords.latitude;
+    var lon = 'lon=' + position.coords.longitude;
+    var urlTarget = endpoint + lat +'&'+ lon;
+    getWeather(urlTarget);
+    console.log(urlTarget);
+    }
+
+  function getWeather(url) {
+    fetch(url, {
+      method: 'GET'
+    }).then(function(response){
+        return response.json();
+      }).then(function(myJson){
+        document.getElementById('location').innerHTML = `${myJson.name}, ${myJson.sys.country}`;
+        document.getElementById('desc').innerHTML = myJson.weather[0].main;
+        document.getElementById('icon').src = getIcon(myJson.weather)
+        document.getElementById('min').innerHTML = myJson.main.temp_min + 'C';
+        document.getElementById('avg').innerHTML = myJson.main.temp + 'C';
+        document.getElementById('max').innerHTML = myJson.main.temp_max + 'C';
+      }).catch(function(error){
+        console.log(error);
+      })
+  }
+
+  function getIcon(weather) {
+    var iconPath, iconValue;
+    var iconArr = weather.filter(function(each){
+      return (each.icon)
+    })
+    iconValue = iconArr[0].icon;
+    iconPath = iconValue.includes('https')? iconValue: `http://openweathermap.org/img/w/${iconValue}.png`
+    console.log(iconPath);
+    return iconPath;
   }
 })();
-
-
-function showPosition (position) {
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-  console.log(lat, long)
-}
--- public/request.js
+++ public/request.js
@@ -1,22 +1,13 @@
-// fetch(url, {
-//   method: 'GET'
-// }).then(function(response) {
-//   return response.json();
-// }).then(function(result){
-//     console.log(result);
-//   }).catch(function(err){
-//   console.log('there is error: ', error);
-// });
-
-
-var testUrl = 'https://api.github.com/users/heathercoraje/repos';
-
-// https://www.sitepoint.com/html5-geolocation/
-fetch(testUrl, {
-  method: 'GET'
-}).then(function(response) {
-  var data = response.json();
-  console.log(data)
-}).catch(function(error){
-  console.log('there is error: ', error);
-});
+function request(url) {
+   fetch(url, {
+    method: 'GET'
+   }).then(function(response) {   // returning a resoloved promise
+    return response.json();
+   }).then(function(myJson){
+    var info = myJson;
+    console.log(info);
+    return info;
+    }).catch(function(error) {
+    console.log(error);
+  });
+}
